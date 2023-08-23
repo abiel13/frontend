d4fc5069c76885baf3522779c9dd784240c6413a
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-08-23 22:37:58 +0100

    routing bug

--- app/auth/login/page.tsx
+++ app/auth/login/page.tsx
@@ -72,7 +72,7 @@ const Login = () => {
       // Handle successful response
       console.log(response);
       toast.success("Login Sucessful", { theme: "colored" });
-      router.push('/')
+      router.push('/comics')
     } catch (errors: any) {
       toast.error(`Error: ${errors.response.data.errors}`, {
         theme: "colored",
