499739fb341ed9e18ebff5bfa44f8e6dd569797a
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-08-24 17:58:46 +0100

    tetst

--- app/comics/page.tsx
+++ app/comics/page.tsx
@@ -1,26 +1,19 @@
 'use client'
-import React from 'react'
-import Navbar from './components/Navbar'
-import Search from './components/Search'
-
-
-<<<<<<< HEAD
- 
+import Link from 'next/link'
+import React , {useState} from 'react'
 
 
 const ComicPage:React.FC =  () => {
   const [categories, setcategories] = useState()
 
-=======
->>>>>>> efcf5f72c9854493e4ff5cd14adcfb755a181d53
   return (
-    <div className='bg-blak min-h-screen px-3 py-[1rem] mt-3 font-bold'>
-<div className="my-4 w-[90%] flex items-center bg-red-400 gap-4 overflow-x-hidden ">
+    <div className='bg-blak    min-h-screen px-3 py-[1rem] mt-3 font-bold'>
+<div className="my-4 w-[90] flex items-center bg-red-400 gap-4 overflow-x-hidden ">
 <Link className='px-5 py-3 rounded-lg bg-white text-black text-center' href={'/'}>Comedy</Link>
 <Link className='px-5 py-3 rounded-lg bg-white text-black text-center' href={'/'}>Epic</Link>
 <Link className='px-5 py-3 rounded-lg bg-white text-black text-center' href={'/'}>Adventure</Link>
 
-  <Link className='px-5 py-3 rounded-lg bg-white text-black text-center' href={'/'}>Flokore</Link>
+  <Link className='px-5 py-3 rounded-lg bg-white text-black text-center' href={'/'}>Folkore</Link>
 </div>
 
    <div>
