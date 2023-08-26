8fc6209b324127d3707e92d54eaf7b5adc6e6f36
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-08-26 14:25:20 +0100

    commit

--- app/comics/components/Navbar.tsx
+++ app/comics/components/Navbar.tsx
@@ -8,7 +8,7 @@ const Navbar = () => {
   return (
     <div style={{borderBottom:'1px solid #444'}} className=' w-full px-3 flex font-bold items-center justify-between py-3'>
 <div className='text-2xl tracking-wider font-bold text-white'>Alte<span className='text-red-700'>Flix</span></div>
-<div style={{background:'red'}} className='w-[50px] h-[50px] rounded-full flex items-center justify-center text-white'>{userData.firstname[0]} . {userData.lastname[0]}</div>
+<div style={{background:'red'}} className='w-[50px] h-[50px] rounded-full flex items-center justify-center text-white'>{userData?.firstname[0]} . {userData?.lastname[0]}</div>
     </div>
   )
 }
