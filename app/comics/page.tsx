2b8cfd0b84af1013c83b2057f43ef0c2ccddd0df
Abiel asimiea <97836551+abiel13@users.noreply.github.com>
2023-09-04 12:57:59 +0100

    loading feature

--- app/comics/page.tsx
+++ app/comics/page.tsx
@@ -4,9 +4,10 @@ import axios from "axios";
 import Image from "next/image";
 import Link from "next/link";
 import React, { Suspense } from "react";
-import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import Skeleton from "react-loading-skeleton";
 import { Stories, Categories } from "../utils";
 import BookCard from "./components/BookCard";
+
 
 interface ComicPageProps {}
 
@@ -35,7 +36,17 @@ const ComicPage: ({}: ComicPageProps) => Promise<JSX.Element> = async ({}) => {
   return (
     <div className="bg-blak flex flex-col items-center md:px-[5rem]   min-h-[100vh] px-3 py-[1rem] mt-3 font-bold">
       <div className="my-4 justify-around w-full md:w-1/2 flex items-center gap-4 cat_over ">
-        {categories?.map((item, index) => (
+
+        {!categories?.length ? <div className='flex gap-5'>
+        { [0,0,0,0]?.map((item) => (
+          <Skeleton
+          width={100}
+          height={50}
+         
+            className={` md:ml-0 px-5 py-3 bg-white rounded-lg  text-black text-center`}
+          /> 
+        ))}
+        </div> : <div>{ categories?.map((item, index) => (
           <Link
             className={`${
               index == 0 ? "ml-[5rem]" : "ml-0"
@@ -45,7 +56,7 @@ const ComicPage: ({}: ComicPageProps) => Promise<JSX.Element> = async ({}) => {
           >
             {<p className="">{item.title}</p>}
           </Link>
-        ))}
+        ))}</div> }
       </div>
 
       <div className="w-full  flex flex-col items-center ">
@@ -54,6 +65,21 @@ const ComicPage: ({}: ComicPageProps) => Promise<JSX.Element> = async ({}) => {
          <BookCard stories={stories} /> 
         </div>
       </div>
+
+      <div className="w-full mt-[2rem]  flex flex-col items-center ">
+        <h1 className="text-2xl text-white tracking-wide  self-start mb-8 ">Characters</h1>
+        <div className="flex items-center justify-between  mt-5 ">
+         <BookCard stories={stories} /> 
+        </div>
+      </div>
+
+      <div className="w-full mt-[2rem]  flex flex-col items-center ">
+        <h1 className="text-2xl text-white tracking-wide  self-start mb-8 ">Other Stories</h1>
+        <div className="flex items-center justify-between  mt-5 ">
+         <BookCard stories={stories} /> 
+        </div>
+      </div>
+
     </div>
   );
 };
