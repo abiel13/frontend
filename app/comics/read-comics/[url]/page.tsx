e107b4a3aca4e587833b9d003c7c71dd8bbb6a22
abiel13 <dbestabi28@gmail.com>
2023-09-22 11:22:17 +0100

    tried read page with cors free pdf

--- app/comics/read-comics/[url]/page.tsx
+++ app/comics/read-comics/[url]/page.tsx
@@ -4,6 +4,10 @@ import { useState , useEffect } from 'react';
 import { Document, Page } from 'react-pdf';
 import { pdfjs } from 'react-pdf';
 import axios from 'axios'
+import 'react-pdf/dist/Page/TextLayer.css';
+import 'react-pdf/dist/Page/AnnotationLayer.css';
+import { Typography } from '@mui/material';
+import { BiChevronLeftCircle, BiChevronRightCircle } from 'react-icons/bi';
 
 pdfjs.GlobalWorkerOptions.workerSrc = new URL(
   'pdfjs-dist/build/pdf.worker.min.js',
@@ -15,7 +19,7 @@ pdfjs.GlobalWorkerOptions.workerSrc = new URL(
 const ReadBookPage = ({params} :{ params: {url : string}})=>{
 
     const [numPages, setNumPages] = useState<number>();
-    const [pageNumber, setPageNumber] = useState<number>(1);
+    const [pageNumber, setPageNumber] = useState<number>(2);
     const [doc_url , setDoc_url] =useState<string>('')
     
     
@@ -31,14 +35,19 @@ setDoc_url(doc_url[0]?.doc_url)
     }
 
     return (
-        <div className='text-black text-3xl'>
-        <Document file={doc_url} onLoadSuccess={onDocumentLoadSuccess}>
+        <div className='w-[100vw] min-h-[80vh] mt-[4rem] flex items-center justify-center flex-col text-3xl'>
+        <Document file={'https://pdftron.s3.amazonaws.com/downloads/pl/demo-annotated.pdf'} onLoadSuccess={onDocumentLoadSuccess}>
           <Page pageNumber={pageNumber} />
         </Document>
-        <p className='text-black'>
+        <div className='flex items-center justify-around gap-5 mt-[1rem]'>
+    <BiChevronLeftCircle fontSize={40} onClick={() =>setPageNumber((prev) => prev -1)} />
+           <Typography className='text-black'>
           Page {pageNumber} of {numPages}
-     
-        </p>
+        </Typography>
+        <BiChevronRightCircle fontSize={40} onClick={() => setPageNumber((prev) => prev +1)} />
+        </div>
+       
+
       </div>
     )
 }
